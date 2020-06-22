54910255ab3e12bdfde9e38ed2532c3e4229a855
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-22 14:52:29 +0100

    Reverting database script

--- db-armada.sql
+++ db-armada.sql
@@ -22,31 +22,6 @@ SET default_tablespace = '';
 
 SET default_with_oids = false;
 
--- Database: db-armada
-
-DROP DATABASE IF EXISTS "db-armada";
-
-CREATE DATABASE "db-armada"
-    WITH
-    OWNER = postgres
-    ENCODING = 'UTF8'
-    LC_COLLATE = 'French_France.1252'
-    LC_CTYPE = 'French_France.1252'
-    TABLESPACE = pg_default
-    CONNECTION LIMIT = -1;
-
--- SCHEMA: public
-
-CREATE SCHEMA public
-    AUTHORIZATION postgres;
-
-COMMENT ON SCHEMA public
-    IS 'standard public schema';
-
-GRANT ALL ON SCHEMA public TO postgres;
-
-GRANT ALL ON SCHEMA public TO PUBLIC;
-
 --
 -- TOC entry 197 (class 1259 OID 16490)
 -- Name: Chauffeur; Type: TABLE; Schema: public; Owner: postgres
