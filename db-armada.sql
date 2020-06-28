cc594565873f471fee775b10c71aae197e562515
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-28 16:37:44 +0100

    Modifying the database script

--- db-armada.sql
+++ db-armada.sql
@@ -138,6 +138,7 @@ ALTER SEQUENCE public.Fichier_id_seq OWNED BY public.Fichier.id;
 
 CREATE TABLE public.Maintenance (
     id bigint NOT NULL,
+    Type character varying(150) NOT NULL,
     Niveau integer NOT NULL,
     Echelon integer NOT NULL,
     Date date,
--- routes/maintenances.js
+++ routes/maintenances.js
@@ -1,8 +1,25 @@
 const express = require('express')
 const router = express.Router()
+const xmlConverter = require('xml-js')
+const configIndex = require('../config/index')
+const { Pool } = require('pg')
+const pool = new Pool({
+    connectionString: configIndex.getDbConnectionString()
+})
+pool.connect()
 
 router.post('/maintenance', async(req,res)=>{
-    //TODO Creer une maintenance
+    const text = "INSERT INTO Maintenance(type, contenu) VALUES('FT',$1) RETURNING id"
+    const values = [data]
+    await pool.query(text, values)
+        .then(result=>{
+            console.log(`Fiche technique successfully added. id: ${result.rows[0].id}`)
+            res.send(result.rows[0].id)
+        })
+        .catch(e => {
+            console.error(e.message)
+            res.send(e.message)
+        })
 })
 
 router.get('/planning', async(req,res)=>{
