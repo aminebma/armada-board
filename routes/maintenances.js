b9238ace87283975e311a02582fd7f645e369797
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-28 04:04:48 +0100

    Deleting 'Fiche de suivi' from the files endpoints and adding some todos to the maintenance route

--- routes/maintenances.js
+++ routes/maintenances.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
 
+router.post('/maintenance', async(req,res)=>{
+    //TODO Creer une maintenance
+})
+
 router.get('/planning', async(req,res)=>{
-    //TODO Générer un planning de maintenance
+    //TODO Exporter un planning de maintenance
+})
+
+router.post('/planning', async(req,res)=>{
+    //TODO Generer un planning
+})
+
+router.delete('/maintenance', async(req,res)=>{
+    //TODO Supprimer une maintenance
+})
+
+router.put('/maintenance', async(req,res)=>{
+    //TODO Modifier une maintenance
 })
 
 module.exports = router
