cd8fe0580d1785dd15f23d8158891790c0cf3ba5
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-06-29 17:42:43 +0100

    Externalizing the appointments modules for a cleaner code.

--- routes/maintenances.js
+++ routes/maintenances.js
@@ -6,13 +6,15 @@ const { Pool } = require('pg')
 const pool = new Pool({
     connectionString: configIndex.getDbConnectionString()
 })
-pool.connect()
 const moment = require('moment')
+pool.connect()
 const ref_motor = [
     'Carburant',
     'Refroidissement',
     'Capacité en huile'
 ]
+
+const Maintenance = require('../modules/Maintenance')
 
 //This will add a new maintainance to the database. the request body should include the type, level, echelon, start date,
 //end date, vehicule id, unity id and the need in this format:
@@ -103,91 +105,14 @@ router.post('/planning', async(req,res)=>{
                     const sorties = req.body.carnet_de_bord.contenu.sortie
 
                     //Checking for the next oil appointment
-                    if(motorInfo[0].type._text === 'Essence'){
-                        //Average distance per day in kilometers
-                        let avgKm = 0.0
-                        for(let sortie of sorties)
-                            avgKm += (sortie.compteur_fin - sortie.compteur_debut)
-                        let nbDays = (moment(sorties[sorties.length-1].date).diff(moment(sorties[0].date), 'days'))
-                        avgKm/=((nbDays===0) ? 1 : nbDays)
-                        //Calculating the remaining distance before the next checkup
-                        const remKm= 10000-(sorties[sorties.length-1].compteur_fin % 10000)
-                        const nextAppointment = moment().add(Math.floor(remKm/avgKm),'days')
-                        //If there is no oil appointment coming
-                        if(moment(maintenances.rows.filter(maintenance => maintenance.type === 'Vidange')[0].date).isBefore(nextAppointment)){
-                            text = "SELECT DISTINCT ON (date_fin::date) date_fin as date\n" +
-                                "FROM Maintenance WHERE date_debut >= $1 \n" +
-                                "ORDER BY date_fin::date, date_fin DESC"
-                            values = [nextAppointment.format('YYYY-MM-DD')]
-                            //Checking for the next free appointment date
-                            await pool.query(text, values)
-                                .then(async dates => {
-                                    //If there is no appointment that day
-                                    if(dates.rows.length === 0 || !moment(dates.rows[0].date).isSame(nextAppointment,'day'))
-                                        nextAppointment.set({'hour':8, 'minutes':0, 'seconds':0})
-                                    else {
-                                        let appointed = false
-                                        let i=0
-                                        while(!appointed){
-                                            if(moment(dates.rows[i].date).hour() === 17) {
-                                                //Avoiding week ends (Friday and Saturday)
-                                                nextAppointment.add(1*((nextAppointment.day()/5)=== 0 ? 1 : 2), 'day')
-                                                if(((i+1)===dates.rows.length) || !nextAppointment.isSame(moment(dates.rows[i+1].date),'day')){
-                                                    nextAppointment.set({'hour': 8, 'minutes':0, 'seconds': 0})
-                                                    appointed=true
-                                                }
-                                                else i++
-                                            }
-                                            else {
-                                                nextAppointment.set({'hour': moment(dates.rows[i].date).hour() + 1, 'minutes': 0, 'seconds': 0})
-                                                appointed=true
-                                            }
-                                        }
-                                    }
-                                    //Getting the needed level and echelon
-                                    text = "SELECT niveau, echelon FROM Ref_maintenance WHERE type='Vidange'"
-                                    await pool.query(text)
-                                        .then(async reference => {
-                                            let besoin = {
-                                                "_declaration":{
-                                                    "_attributes":{
-                                                        "version": "1.0",
-                                                        "encoding": "utf-8"
-                                                    }
-                                                },
-                                                "contenu":{
-                                                    "piece":[
-                                                        {
-                                                            "modele":"Huile",
-                                                            "quantite": parseInt(motorInfo[2].mesure._text)
-                                                        }
-                                                    ]
-                                                }
-                                            }
-                                            besoin = await xmlConverter.json2xml(besoin, {compact: true, spaces: '\t'})
-                                            const appointment = {
-                                                type: "Vidange",
-                                                niveau: reference.rows[0].niveau,
-                                                echelon: reference.rows[0].echelon,
-                                                date_debut: nextAppointment.format('YYYY-MM-DD HH:mm:ss'),
-                                                date_fin: nextAppointment.hour(nextAppointment.hour()+1).format('YYYY-MM-DD HH:mm:ss'),
-                                                vehicule: maintenances.rows[0].id_vehicule,
-                                                besoin: besoin
-                                            }
-                                            planning.push(appointment)
-                                        })
-                                        .catch(e => {
-                                            console.error(e.message)
-                                            res.send(e.message)
-                                        })
-                                })
-                                .catch(e => {
-                                    console.error(e.message)
-                                    res.send(e.message)
-                                })
-                        }
-                    }
-                    console.log(motorInfo)
+                    await Maintenance.generateOilAppointment(sorties, maintenances, motorInfo)
+                        .then(appointment => {
+                            planning.push(appointment)
+                        })
+                        .catch(e => {
+                            console.error(e.message)
+                            res.send(e.message)
+                        })
                     res.send({ Message: 'Done'})
                 })
                 .catch(e => {
