04643f39baa48eca450a9f9dadbd1dbd0b9e93e9
Amine BENBAKHTA <gm_benbakhta@esi.dz>
2020-07-07 23:57:10 +0100

    Still implementing login

--- View/src/Layouts/Login.js
+++ View/src/Layouts/Login.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -15,26 +15,13 @@ import Container from '@material-ui/core/Container';
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 import axios from 'axios';
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {'Copyright © '}
-      <Link color="inherit" href="https://www.github.com/aminebma/armada-board">
-        Armada-Board
-      </Link>{' '}
-      {new Date().getFullYear()}
-      {'.'}
-    </Typography>
-  );
-}
-
 const theme = createMuiTheme({
-    palette: {
-      secondary: {
-        main: '#2196f3'
-      }
+  palette: {
+    secondary: {
+      main: '#2196f3'
     }
-  });
+  }
+});
 
 const useStyles = makeStyles((theme) => ({
   paper: {
@@ -56,87 +43,109 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const handleLogin = () => {
-  console.log('hey1')
-  const body = {
-    username: document.getElementById('username').value,
-    password: document.getElementById('password').value
+class Login extends Component{
+  constructor(props){
+    super(props)
+    this.handleLogin = this.handleLogin.bind(this)
+    this.Copyright = this.Copyright.bind(this)
   }
-  console.log('hey2')
-  console.log(body)
-  axios.post('http://localhost:3001/api/accounts/sign-in', body)
-      .then(res => {
-            console.log('hey3')
-            alert(res.data)
-      })
-};
 
-export default function Login() {
-  const classes = useStyles();
-  return (
-    <MuiThemeProvider theme={theme}>
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
+
+  handleLogin = () => {
+    console.log('hey1')
+    const body = {
+      username: document.getElementById('username').value,
+      password: document.getElementById('password').value
+    }
+    console.log('hey2')
+    console.log(body)
+    axios.post('http://localhost:3001/api/accounts/sign-in', body)
+        .then(res => {
+          this.props.navigation(true,res.data)
+          alert('7chinah')
+        })
+  };
+
+  Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+        {'Copyright © '}
+        <Link color="inherit" href="https://www.github.com/aminebma/armada-board">
+        Armada-Board
+        </Link>{' '}
+    {new Date().getFullYear()}
+    {'.'}
+  </Typography>
+  );
+  }
+
+  render(){
+    const classes = useStyles();
+    return (
+        <MuiThemeProvider theme={theme}>
+        <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
         <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
+        <LockOutlinedIcon />
         </Avatar>
         <Typography component="h1" variant="h5">
-          Connectez-vous 
+        Connectez-vous
         </Typography>
         <form className={classes.form} noValidate>
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            id="username"
-            label="Username"
-            name="username"
-            autoComplete="username"
-            autoFocus
-            color="secondary"
-          />
-          <TextField
-            variant="outlined"
-            margin="normal"
-            required
-            fullWidth
-            name="password"
-            label="Mot de passe"
-            type="password"
-            id="password"
-            autoComplete="current-password"
-            color="secondary"
-          />
-          <FormControlLabel
-            control={<Checkbox value="remember" color="secondary" />}
-            label="Se souvenir de moi"
-          />
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-            style={{background : '#2196f3'}}
-            onClick={handleLogin}
-          >
-            Se connecter
-          </Button>
-          <Grid container>
-            <Grid item xs>
-              <Link href="#" variant="body2" style={{color : '#2196f3'}}>
-                Mot de passe oublié ?
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      <Box mt={8}>
+    <TextField
+    variant="outlined"
+    margin="normal"
+    required
+    fullWidth
+    id="username"
+    label="Username"
+    name="username"
+    autoComplete="username"
+    autoFocus
+    color="secondary"
+        />
+        <TextField
+    variant="outlined"
+    margin="normal"
+    required
+    fullWidth
+    name="password"
+    label="Mot de passe"
+    type="password"
+    id="password"
+    autoComplete="current-password"
+    color="secondary"
+        />
+        <FormControlLabel
+    control={<Checkbox value="remember" color="secondary" />}
+    label="Se souvenir de moi"
+        />
+        <Button
+    fullWidth
+    variant="contained"
+    color="primary"
+    style={{background : '#2196f3'}}
+    onClick={handleLogin}
+        >
+        Se connecter
+    </Button>
+    <Grid container>
+    <Grid item xs>
+    <Link href="#" variant="body2" style={{color : '#2196f3'}}>
+    Mot de passe oublié ?
+  </Link>
+    </Grid>
+    </Grid>
+    </form>
+    </div>
+    <Box mt={8}>
         <Copyright />
-      </Box>
-    </Container>
-  </MuiThemeProvider>
+        </Box>
+        </Container>
+        </MuiThemeProvider>
   );
+  }
 }
+
+export default {Login}
